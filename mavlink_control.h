03a87cfd8b833d25af7fa8d0436ea06526b0f665
Hannes Diethelm <hdiethel@hsr.ch>
2018-10-18 07:58:59 +0200

    Autotakeoff flag

--- mavlink_control.h
+++ mavlink_control.h
@@ -80,9 +80,9 @@ using namespace std;
 int main(int argc, char **argv);
 int top(int argc, char **argv);
 
-void commands(Autopilot_Interface &autopilot_interface);
+void commands(Autopilot_Interface &autopilot_interface, bool autotakeoff);
 void parse_commandline(int argc, char **argv, char *&uart_name, int &baudrate,
-		bool &use_udp, char *&udp_ip, int &rx_port, int &tx_port);
+		bool &use_udp, char *&udp_ip, int &rx_port, int &tx_port, bool &autotakeoff);
 
 // quit handler
 Autopilot_Interface *autopilot_interface_quit;
